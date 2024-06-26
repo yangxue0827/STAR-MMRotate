db7d2a4f97737399a0b9f72a0af85b351704dced
yangxue0827 <yangxue0827@126.com>
2024-06-26 23:03:16 +0800

    update h2rbox config

--- configs/h2rbox/h2rbox_r50_fpn_1x_dota_le90_adamw5e-5.py
+++ configs/h2rbox/h2rbox_r50_fpn_1x_dota_le90_adamw5e-5.py
@@ -1,12 +1,14 @@
 _base_ = [
-    '../_base_/datasets/dotav1.py', '../_base_/schedules/schedule_1x.py',
+    '../_base_/datasets/dotav1.py',
+    '../_base_/schedules/schedule_1x.py',
     '../_base_/default_runtime.py'
 ]
 angle_version = 'le90'
 
 # model settings
 model = dict(
-    type='H2RBoxV2PDetector',
+    type='H2RBox',
+    crop_size=(1024, 1024),
     backbone=dict(
         type='ResNet',
         depth=50,
@@ -27,7 +29,7 @@ model = dict(
         num_outs=5,
         relu_before_extra_convs=True),
     bbox_head=dict(
-        type='H2RBoxV2PHead',
+        type='H2RBoxHead',
         num_classes=15,
         in_channels=256,
         stacked_convs=4,
@@ -37,9 +39,10 @@ model = dict(
         center_sample_radius=1.5,
         norm_on_bbox=True,
         centerness_on_reg=True,
-        square_cls=[1, 9, 11],
-        resize_cls=[1],
-        scale_angle=False,
+        separate_angle=False,
+        scale_angle=True,
+        reassigner='one2one',
+        rect_classes=[9, 11],
         bbox_coder=dict(
             type='DistanceAnglePointCoder', angle_version=angle_version),
         loss_cls=dict(
@@ -49,10 +52,14 @@ model = dict(
             alpha=0.25,
             loss_weight=1.0),
         loss_bbox=dict(type='IoULoss', loss_weight=1.0),
+        loss_bbox_aug=dict(
+            type='H2RBoxLoss',
+            loss_weight=0.4,
+            center_loss_cfg=dict(type='L1Loss', loss_weight=0.0),
+            shape_loss_cfg=dict(type='IoULoss', loss_weight=1.0),
+            angle_loss_cfg=dict(type='L1Loss', loss_weight=1.0)),
         loss_centerness=dict(
-            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_ss_symmetry=dict(
-            type='SmoothL1Loss', loss_weight=0.2, beta=0.1)),
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
     # training and testing settings
     train_cfg=None,
     test_cfg=dict(
@@ -67,6 +74,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='FilterNoCenterObject', img_scale=(1024, 1024), crop_size=(1024, 1024)),
     dict(type='RResize', img_scale=(1024, 1024)),
     dict(
         type='RRandomFlip',
@@ -74,12 +82,27 @@ train_pipeline = [
         direction=['horizontal', 'vertical', 'diagonal'],
         version=angle_version),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+    dict(type='Pad', size_divisor=1),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
 
-data_root = 'data/split_1024_dota1_0/'
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='RResize'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=64),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+
+data_root = '/data/nas/dataset_share/DOTA/split_ss_dota1_0/'
 data = dict(
     train=dict(type='DOTAWSOODDataset', pipeline=train_pipeline,
                ann_file=data_root + 'trainval/annfiles/',
